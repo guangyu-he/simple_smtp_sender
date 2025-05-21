c89e8759be1f67ad9e3806842c4bac08ca4713e9
Guangyu He <guangyu.he@golden-tech.de>
2025-05-21 13:39:54 +0200

    refactor(email): rename `from_email` to `sender_email` for clarity

--- src/email.rs
+++ src/email.rs
@@ -14,7 +14,7 @@ use pyo3::{pyclass, pymethods};
 #[pyclass(dict, get_all, set_all, str, subclass)]
 pub struct EmailConfig {
     pub server: String,
-    pub from_email: String,
+    pub sender_email: String,
     pub username: String,
     pub password: String,
 }
@@ -26,7 +26,7 @@ impl EmailConfig {
     pub fn new(server: &str, sender_email: &str, username: &str, password: &str) -> Self {
         EmailConfig {
             server: server.to_string(),
-            from_email: sender_email.to_string(),
+            sender_email: sender_email.to_string(),
             username: username.to_string(),
             password: password.to_string(),
         }
@@ -37,8 +37,8 @@ impl fmt::Display for EmailConfig {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "EmailConfig<server={}, from_email={}, username={}, password={}>",
-            self.server, self.from_email, self.username, self.password
+            "EmailConfig<server={}, sender_email={}, username={}, password={}>",
+            self.server, self.sender_email, self.username, self.password
         )
     }
 }
